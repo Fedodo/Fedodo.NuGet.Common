f6b1aa16140cdea01bf9ea998bb2628fd34369f5
Lukas Nagel <lukas@lna-dev.net>
2023-08-08 06:18:44 +0200

    🗃️ Updated to Actor Outbox and Inbox

--- Fedodo.NuGet.Common/Constants/DatabaseLocations.cs
+++ Fedodo.NuGet.Common/Constants/DatabaseLocations.cs
@@ -34,57 +34,13 @@ public static class DatabaseLocations
         Collection = "Users"
     };
 
-    public static DatabaseCollectionPair OutboxFollow { get; } = new()
+    public static DatabaseCollectionPair Outbox { get; } = new()
     {
-        Database = "Outbox",
-        Collection = "Follow"
-    };
-
-    public static DatabaseCollectionPair OutboxLike { get; } = new()
+        Database = "Outbox"
+    };    
+    
+    public static DatabaseCollectionPair Inbox { get; } = new()
     {
-        Database = "Outbox",
-        Collection = "Like"
-    };
-
-    public static DatabaseCollectionPair OutboxAnnounce { get; } = new()
-    {
-        Database = "Outbox",
-        Collection = "Announce"
-    };
-
-    public static DatabaseCollectionPair OutboxCreate { get; } = new()
-    {
-        Database = "Outbox",
-        Collection = "Create"
-    };
-
-    public static DatabaseCollectionPair InboxCreate { get; } = new()
-    {
-        Database = "Inbox",
-        Collection = "Create"
-    };
-
-    public static DatabaseCollectionPair InboxAnnounce { get; } = new()
-    {
-        Database = "Inbox",
-        Collection = "Announce"
-    };
-
-    public static DatabaseCollectionPair InboxAccept { get; } = new()
-    {
-        Database = "Inbox",
-        Collection = "Accept"
-    };
-
-    public static DatabaseCollectionPair InboxFollow { get; } = new()
-    {
-        Database = "Inbox",
-        Collection = "Follow"
-    };
-
-    public static DatabaseCollectionPair InboxLike { get; } = new()
-    {
-        Database = "Inbox",
-        Collection = "Like"
+        Database = "Inbox"
     };
 }
